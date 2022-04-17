310230263e27d20e038f00c04a8c30eec29becde
fatamatuzjaharasamiha <samirajahara843@gmail.com>
2022-04-17 14:03:14 +0600

    create carouset

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 
 const Header = () => {
     return (
         <div>
-            <h1>this is header</h1>
+            <Navbar collapseOnSelect expand="lg" sticky='top' bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand href="/home">Home</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="/blogs">Blogs</Nav.Link>
+                            <Nav.Link href="/about">About</Nav.Link>
+
+                        </Nav>
+                        <Nav>
+                            <Nav.Link href="#deets">Sign up</Nav.Link>
+                            <Nav.Link eventKey={2} href="#memes">
+                                Sign in
+                            </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
         </div>
     );
 };
